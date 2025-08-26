8927970ebacbe690ff4ada71c67f7be30549c932
saykot biswas shawon <shawon.saykot2023@gmail.com>
2025-08-27 01:05:08 +0600

    updating feture engineering file

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,1 +1,12 @@
-1t 
+
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+from typing import Tuple
+import os
+
+VECT_PATH = "vectorizer.pkl"
+
+def fit_vectorizer(corpus, max_features: int = 5000) -> TfidfVectorizer:
+    vec = TfidfVectorizer(max_features=max_features, ngram_range=(1,2))
+    vec.fit(corpus)
+    return vec
